b34badc1acf2abe470e7bea34eb9da6471ae0726
Ugur Emre Dogan <ue.dogan@gmail.com>
2026-09-16 18:55:13 -0700

    We came upto response status codes

--- src/app/main.py
+++ src/app/main.py
@@ -1,16 +1,38 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
 
-app = FastAPI(
-    description="This is a sample FastAPI application.",
-    version="0.0.1",
-)
+app = FastAPI()
 
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello World"}
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: str | None = None
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserInDB(UserOut):
+    hashed_password: str
+
+
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
+
+
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.model_dump(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
+
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
